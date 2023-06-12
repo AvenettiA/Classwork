7fbfb58bc75be324aafa504831d379bf8fc9510d
AvenettiA <133554331+AvenettiA@users.noreply.github.com>
2023-06-12 08:20:50 -0700

    Add files via upload

--- code.js
+++ code.js
@@ -15,7 +15,6 @@ document.getElementById("die2Res").innerHTML = die2;
 var diceSum = die1 + die2;
 console.log("The sume of the dice is:" + diceSum);
 document.getElementById("diceSum").innerHTML = diceSum;
-
     //step three: does the sum equal 7 or 11, meaning are you a loser
 if(diceSum==7||diceSum==11){
     document.getElementById("gameRes").innerHTML = "Ha ha! You lose, loser";
@@ -27,4 +26,44 @@ if(diceSum==7||diceSum==11){
     document.getElementById("gameresult").innerHTML = 
     "You did not lose, You did win, be cool and please play again!.";
 }
+}
+
+//checkCreds funtion for strings.html
+function checkCreds(){
+    console.log("checkCreds() started...");
+    document.getElementById("loginStatus").innerHTML = "Status of login";
+var firstName;
+var lastName;
+var zipCode;
+var fullName;
+var fullNameLength;
+
+//this will place the characters under the fName id in the firstname var. 
+firstName = document.getElementById("fName").value;
+console.log("The first name is inputed as " + firstName);
+
+lastName = document.getElementById("lName").value;
+console.log("The last name is inputed as " + lastName);
+
+zipCode = document.getElementById("zipCode").value;
+console.log("The zip code is inputed as " + zipCode);
+
+fullName = firstName + " " + lastName;
+console.log("The full name is inputed as " + fullName);
+
+fullNameLength = fullName.length;
+console.log("The full name has " + fullNameLength + " characters");
+
+zipCodeNumb = parseInt(zipCode);
+console.log("The zip code number is " + zipCodeNumb);
+
+if(fullNameLength > 20){
+    document.getElementById("loginStatus").innerHTML = "Invalid name, please try again";
+} else if (zipCode.length != 5){
+    document.getElementById("loginStatus").innerHTML = "Invalid zip code, please try again";
+} else{
+    alert("User credientals passed, welcome to the site, " + fullName);
+}
+
+
 }
